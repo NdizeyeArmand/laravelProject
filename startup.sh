8b50f835d1db9dfebb7f720df7026e03d91ba783
Dark_Loon <armandgaeln@gmail.com>
2026-04-18 01:45:47 +0200

    Fix nginx document root in startup script

--- startup.sh
+++ startup.sh
@@ -4,6 +4,10 @@ set -e
 echo "=== Azure Startup Script ==="
 cd /home/site/wwwroot
 
+# Fix nginx document root to point at Laravel's public/ folder
+sed -i 's|root /home/site/wwwroot;|root /home/site/wwwroot/public;|g' /etc/nginx/sites-available/default
+nginx -s reload
+
 php artisan config:clear
 php artisan migrate --force
 php artisan config:cache
