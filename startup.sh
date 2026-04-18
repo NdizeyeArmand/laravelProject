5aa69fb9853555e462c31f276c7b60a213841148
Dark_Loon <armandgaeln@gmail.com>
2026-04-18 12:09:24 +0200

    Write my own Azure nginx config in startup script

--- startup.sh
+++ startup.sh
@@ -6,7 +6,15 @@ cd /home/site/wwwroot
 
 # Fix nginx document root to point at Laravel's public/ folder
 sed -i 's|root /home/site/wwwroot;|root /home/site/wwwroot/public;|g' /etc/nginx/sites-available/default
-nginx -s reload
+
+# Add try_files to the primary location block if not already present
+# This routes all requests through Laravel's index.php front controller
+if ! grep -q "try_files" /etc/nginx/sites-available/default; then
+    sed -i 's|index  index.php index.html index.htm;|index  index.php index.html index.htm;\n        try_files $uri $uri/ /index.php?$query_string;|g' \
+        /etc/nginx/sites-available/default
+fi
+
+nginx -t && nginx -s reload
 
 php artisan config:clear
 php artisan migrate --force
